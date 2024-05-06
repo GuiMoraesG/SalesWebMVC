daf06599078e3fbb088cc4c53f2f62a8b9988710
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-05-06 12:47:33 -0300

    Organização

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Sellers}/{action=Index}/{id?}");
 
 app.Run();
